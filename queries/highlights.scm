939572d044ba5bce066b7a72e92d4ab687f813a8
Jan-Hendrik Matthes <matthesjh@gmail.com>
2020-04-19 15:42:31 +0200

    Add some more comments to the highlights file

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,33 +1,40 @@
 ; Types
+; -----
 
 (module_identifier) @type
 (type_identifier) @type
 
 ; Constructors
+; ------------
 
 (constructor_identifier) @constructor
 
 ; Functions
+; ---------
 
 (label (variable_identifier) @function)
 (variable_identifier) @function
 
 ; Operators
+; ---------
 
 (label (variable_operator) @operator)
 (variable_operator) @operator
 
 ; Numbers
+; -------
 
 (int) @number
 
 ; Comments
+; --------
 
 (cpp_directive) @comment
 (comment) @comment
 (pragma) @comment
 
 ; Keywords
+; --------
 
 "as" @keyword
 "hiding" @keyword
@@ -40,6 +47,7 @@
 "where" @keyword
 
 ; Punctuation
+; -----------
 
 "(" @punctuation.bracket
 ")" @punctuation.bracket
