7a8c0694d06c4d9e0ba57b6d46788d90b0055148
Jan-Hendrik Matthes <matthesjh@gmail.com>
2020-05-01 16:59:10 +0200

    Complete the highlights file

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,60 +1,128 @@
+; Keywords
+; --------
+
+"as" @keyword
+"case" @keyword
+"class" @keyword
+"data" @keyword
+"default" @keyword
+"deriving" @keyword
+"do" @keyword
+"else" @keyword
+"external" @keyword
+"fcase" @keyword
+"forall" @keyword
+"free" @keyword
+"hiding" @keyword
+"if" @keyword
+"import" @keyword
+"in" @keyword
+"infix" @keyword
+"infixl" @keyword
+"infixr" @keyword
+"instance" @keyword
+"let" @keyword
+"module" @keyword
+"newtype" @keyword
+"of" @keyword
+"qualified" @keyword
+"then" @keyword
+"type" @keyword
+"where" @keyword
+
+".." @keyword
+"::" @keyword
+"=" @keyword
+"\" @keyword
+"|" @keyword
+"<-" @keyword
+"->" @keyword
+"@" @keyword
+"~" @keyword
+"=>" @keyword
+
+; Classes
+; -------
+
+(module_identifier) @class
+
 ; Types
 ; -----
 
-(module_identifier) @type
 (type_identifier) @type
+(class_identifier) @type
+(type_variable_identifier) @type
+(simple_type "_") @type
+(simple_type_expression "_") @type
+(type_constructor "()") @type.builtin
+(type_constructor "[]") @type.builtin
+(type_constructor "(->)") @type.builtin
+(type_constructor (tuple_constructor)) @type.builtin
 
 ; Constructors
 ; ------------
 
-(constructor_identifier) @constructor
+(data_constructor_identifier) @constructor
+(general_data_constructor "()") @constructor.builtin
+(general_data_constructor "[]") @constructor.builtin
+(general_data_constructor (tuple_constructor)) @constructor.builtin
 
 ; Functions
 ; ---------
 
-(label (variable_identifier) @function)
-(variable_identifier) @function
+(function_identifier) @function
+
+; Properties
+; ----------
+
+(label_identifier) @property
+
+; Variables
+; ---------
+
+(variable_identifier) @variable
+(simple_pattern "_") @variable
+(basic_expression "_") @variable
 
 ; Operators
 ; ---------
 
-(label (variable_operator) @operator)
-(variable_operator) @operator
+(infix_operator) @operator
+":" @operator.builtin
+"-" @operator.builtin
 
 ; Numbers
 ; -------
 
 (int) @number
+(float) @number
+
+; Strings
+; -------
+
+(char) @string
+(string) @string
 
 ; Comments
 ; --------
 
+(pragma) @comment
 (cpp_directive) @comment
 (comment) @comment
-(pragma) @comment
-
-; Keywords
-; --------
-
-"as" @keyword
-"hiding" @keyword
-"import" @keyword
-"infix" @keyword
-"infixl" @keyword
-"infixr" @keyword
-"module" @keyword
-"qualified" @keyword
-"where" @keyword
 
 ; Punctuation
 ; -----------
+
+(all_constructors) @punctuation
 
 "(" @punctuation.bracket
 ")" @punctuation.bracket
 "{" @punctuation.bracket
 "}" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
 "`" @punctuation.bracket
-(all_constructors) @punctuation.bracket
 
-"," @punctuation.delimiter
 (terminal) @punctuation.delimiter
+"," @punctuation.delimiter
+"." @punctuation.delimiter
