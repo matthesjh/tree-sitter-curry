a32db2aff022adca6df9fbddf379e2f0e43cae91
Jan-Hendrik Matthes <matthesjh@gmail.com>
2025-10-29 00:11:18 +0100

    Generate parser with ABI 14 to fix tests

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 15
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 1734
 #define LARGE_STATE_COUNT 51
 #define SYMBOL_COUNT 249
@@ -5001,7 +5001,7 @@ static bool ts_lex_keywords(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0, .external_lex_state = 1},
   [1] = {.lex_state = 0, .external_lex_state = 2},
   [2] = {.lex_state = 4},
@@ -66751,7 +66751,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_curry(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
-    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -66777,13 +66776,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_curry(void) {
       tree_sitter_curry_external_scanner_deserialize,
     },
     .primary_state_ids = ts_primary_state_ids,
-    .name = "curry",
-    .max_reserved_word_set_size = 0,
-    .metadata = {
-      .major_version = 0,
-      .minor_version = 0,
-      .patch_version = 3,
-    },
   };
   return &language;
 }
