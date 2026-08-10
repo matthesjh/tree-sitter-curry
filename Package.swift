abd948834a1fde87296a703fec5e49975a89e0ba
Jan-Hendrik Matthes <matthesjh@gmail.com>
2026-08-10 22:21:53 +0200

    Update tree-sitter-cli to version 0.26.12

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "TreeSitterCurry", targets: ["TreeSitterCurry"]),
     ],
     dependencies: [
-        .package(name: "SwiftTreeSitter", url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.9.0"),
+        .package(url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.10.0"),
     ],
     targets: [
         .target(
@@ -31,7 +31,7 @@ let package = Package(
         .testTarget(
             name: "TreeSitterCurryTests",
             dependencies: [
-                "SwiftTreeSitter",
+                .product(name: "SwiftTreeSitter", package: "swift-tree-sitter"),
                 "TreeSitterCurry",
             ],
             path: "bindings/swift/TreeSitterCurryTests"
